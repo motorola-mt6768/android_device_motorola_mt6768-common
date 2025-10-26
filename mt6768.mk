4407b2b6ccb367881e7933cef481eb9f2aa5826d
HollowAOSP <ahmedshahinx0x@gmail.com>
2025-10-26 13:48:46 -0400

    mt6768-common: Build Required HIDL interfaces
    Change-Id: I6b444c13615df7fb7807246f93e742cb602edc7a

--- mt6768.mk
+++ mt6768.mk
@@ -179,11 +179,20 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.allocator@1.0 \
+    android.hidl.base@1.0.vendor \
+    android.hidl.allocator@1.0.vendor \
     libflatbuffers-cpp.vendor \
     libhidltransport \
     libhidltransport.vendor \
     libhwbinder\
     libhwbinder.vendor \
+    libruy.vendor \
+    libpcap.vendor
+
+PRODUCT_PACKAGES += \
+    libflatbuffers-cpp.vendor \
     libruy.vendor \
     libpcap.vendor
 
