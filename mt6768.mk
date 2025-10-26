ad667e8d89f13ac87528047e1c72393876f3bdf3
bengris32 <bengris32@protonmail.ch>
2025-10-26 06:56:53 -0400

    mt6768-common: Switch BtAudio to AIDL
    Test: m
    Bug: 203490261
    Change-Id: I65e8375ed5d680060210ab7168a0dc513031900c

--- mt6768.mk
+++ mt6768.mk
@@ -49,7 +49,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.0.vendor \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
-    android.hardware.bluetooth.audio@2.1-impl \
+    android.hardware.bluetooth.audio-impl \
     android.hardware.audio.common-util \
     android.hardware.audio.common@7.0.vendor \
     android.hardware.audio.common@7.0-util.vendor \
